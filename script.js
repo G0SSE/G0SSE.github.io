a765d2d3ed01edbdcfb26e64897df493aeacc63c
Gus Kreydt <91036989+G0SSE@users.noreply.github.com>
2023-10-10 20:20:16 +0200

    chatgpt query works

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// script.js
 function sendMessage() {
     var userMessage = document.getElementById("userMessage").value;
     var chatBox = document.querySelector(".chat-box");
@@ -7,12 +6,38 @@ function sendMessage() {
     message.innerHTML = "<p>" + userMessage + "</p>";
     chatBox.appendChild(message);
     document.getElementById("userMessage").value = "";
-
-    // Simulate a response from TechGPT (you can replace this with actual API calls)
-    setTimeout(function () {
+  
+    fetch('https://api.openai.com/v1/chat/completions', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer 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'
+      },
+      body: JSON.stringify({
+        model: 'gpt-3.5-turbo',
+        max_tokens: 50,
+        n: 1,
+        stop: '\n',
+        messages: [{ "role": "user", "content": userMessage }]
+      })
+    })
+    .then(response => response.json())
+    .then(data => {
+      console.log('API Response:', data); // Log the API response for troubleshooting
+      if (data.choices && data.choices.length > 0) {
+        var responseMessage = data.choices[0].message.content;
         var response = document.createElement("div");
         response.classList.add("message", "received");
-        response.innerHTML = "<p>Here's your answer from TechGPT.</p>";
+        response.innerHTML = "<p>" + responseMessage + "</p>";
         chatBox.appendChild(response);
-    }, 1000);
-}
+      } else if (data.error) {
+        console.error('API Error:', data.error.message);
+      } else {
+        console.error('Error: Invalid API response', data);
+      }
+    })
+    .catch(error => {
+      console.error('Fetch Error:', error);
+    });
+  }
+  
